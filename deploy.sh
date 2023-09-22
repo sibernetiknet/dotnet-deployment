bebff9da3c18c8a8046088c55b9e422f44516b55
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2023-09-22 14:49:05 +0300

    Added project path reading.

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,41 @@
+#Path variables
+HOME_DIRECTORY=~
+
 #Functions
 PRINTMESSAGE(){
-    echo "[*] $1"
+    echo -e "\n [*] $1 \n"
 }
 LOGMESSAGE(){
-    echo "[**] Log: $1"
+    echo -e "\n [**] Log: $1 \n"
 }
 
 SDKCHECK(){
     if command -v dotnet >/dev/null 2>&1; then
-    printMessage "The .NET SDK is installed."
+    PRINTMESSAGE "The .NET SDK is installed."
     else
-    printMessage "The .NET SDK is not installed. Visit: https://dotnet.microsoft.com/en-us/download"
+    PRINTMESSAGE "The .NET SDK is not installed. Visit: https://dotnet.microsoft.com/en-us/download"
     exit
     fi
 }
 
-
-
+#.NET SDK Control
 SDKCHECK
+
+
+while true; do
+  read -p "Enter the project full path of the folder: " PROJECT_PATH
+
+  if [ -d "$PROJECT_PATH" ]; then
+    LOGMESSAGE "Folder exist."
+    break
+  else
+    PRINTMESSAGE "Folder does not exist: $PROJECT_PATH"
+  fi
+done
+
+
+
+
 
 
 
